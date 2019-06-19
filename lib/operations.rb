cf0fa96aeca06ec85aa80dd1e56b7fb8cfcde37a
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-06-19 20:23:16 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def unsafe?(speed)
   
   if speed > 60
@@ -14,3 +15,25 @@ def not_safe?(speed)
   speed < 40 || speed > 60 ? true : false
   
 end
+=======
+def unsafe?(speed)
+  
+speed == 79
+if speed > 60
+  return true
+ end
+if speed < 40
+  return true
+ else 
+   return false
+ end
+end
+
+
+
+def not_safe?(speed)
+  speed == 79
+  speed > 60 < 40 ? true : false
+  end
+  
+>>>>>>> 005e9cdc3368f2442e140251b72961f4b4a590ec
